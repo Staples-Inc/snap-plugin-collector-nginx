072eabdf33dcd4a0e1c602a5186abc3524fd21a7
Lisi, Jacob <jacob.t.lisi@gmail.com>
2016-06-24 13:54:55 -0400

    Hotfix to switch plugin array so it works

--- nginx/nginx.go
+++ nginx/nginx.go
@@ -1,293 +1,294 @@
-
 package nginx
 
 import (
-"fmt"
-"net/http"
-"io/ioutil"
-"errors"
-"encoding/json"
-"strings"
-"time"
-"net"
-"strconv"
-"crypto/md5"
-"encoding/hex"
-"reflect"
-
-
-"github.com/intelsdi-x/snap/control/plugin"
-"github.com/intelsdi-x/snap/control/plugin/cpolicy"
-"github.com/intelsdi-x/snap/core"
-"github.com/intelsdi-x/snap/core/ctypes"
-) 
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	// "github.com/intelsdi-x/snap-plugin-utilities/ns"
+	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/core"
+	"github.com/intelsdi-x/snap/core/ctypes"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
 
 const (
-// Name of plugin
-    Name = "nginx"
-// Version of plugin
-    Version = 1
-// Type of plugin
-    Type = plugin.CollectorPluginType
+	// Name of plugin
+	Name = "nginx"
+	// Version of plugin
+	Version = 1
+	// Type of plugin
+	Type = plugin.CollectorPluginType
 )
 
 var (
-    errNoWebserver  = errors.New("nginx_status_url config required. Check your config JSON file")
-    errBadWebserver = errors.New("Failed to parse given nginx_status_url")
-    errReqFailed    = errors.New("Request to nginx webserver failed")
-    errConfigReadError = errors.New("Config read Error")
-    ) 
+	errNoWebserver     = errors.New("nginx_status_url config required. Check your config JSON file")
+	errBadWebserver    = errors.New("Failed to parse given nginx_status_url")
+	errReqFailed       = errors.New("Request to nginx webserver failed")
+	errConfigReadError = errors.New("Config read Error")
+)
 
-// make sure that we actually satisify requierd interface
+// make sure that we actually satisify required interface
 var _ plugin.CollectorPlugin = (*Nginx)(nil)
 
-type Nginx struct {}	
-
-
+type Nginx struct{}
 
 func getMD5Hash(text string) string {
-    hasher := md5.New()
-    hasher.Write([]byte(text))
-    return hex.EncodeToString(hasher.Sum(nil))
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
 
+func getHostName(inData interface{}, hostName string) string {
+	hostName = fmt.Sprintf("host_id_%s", hostName)
+	switch mtype := inData.(type) {
+	case map[string]interface{}:
+		val := mtype["server"].(string)
+		//Default hostname with port will be encoded to md5
+		//If LookUpAddr get valid hostname it will be replace
+		hostName = fmt.Sprintf("host_id_%s", getMD5Hash(val))
 
-func getHostName( inData interface{}, hostName string)(string) {
-    hostName = fmt.Sprintf("host_id_%s",hostName)
-    switch mtype := inData.(type) {
-    case map[string]interface{}:
-        val := mtype["server"].(string)
-        //Default hostname with port will be encoded to md5
-        //If LookUpAddr get valid hostname it will be replace
-        hostName = fmt.Sprintf("host_id_%s",getMD5Hash(val))
-
-        //check for IPV4
-        if strings.Count(val,".") == 3 {
-            subStr := strings.Split(val,":")
-            hName,err := net.LookupAddr(subStr[0])
-            if err == nil {
-                hostName = strings.Join(hName,".")
-            }
-        }else {
-            if strings.Contains(val,"::") == true {
-                subStr := strings.Split(val,"]")
-                tStr := strings.TrimLeft(subStr[0],"[")
-                hName,err := net.LookupAddr(tStr)
-                if err == nil {
-                    hostName = strings.Join(hName,".")
-                }
-            }
-        }
-    }
-    hostName = strings.TrimRight(hostName,".")
-    replacer := strings.NewReplacer(".", "_", "/", "_", "\\", "_", ":", "_", "%", "_" )
-    hostName = replacer.Replace(hostName)
-    return hostName
+		//check for IPV4
+		if strings.Count(val, ".") == 3 {
+			subStr := strings.Split(val, ":")
+			hName, err := net.LookupAddr(subStr[0])
+			if err == nil {
+				hostName = strings.Join(hName, ".")
+			}
+		} else {
+			if strings.Contains(val, "::") == true {
+				subStr := strings.Split(val, "]")
+				tStr := strings.TrimLeft(subStr[0], "[")
+				hName, err := net.LookupAddr(tStr)
+				if err == nil {
+					hostName = strings.Join(hName, ".")
+				}
+			}
+		}
+	}
+	hostName = strings.TrimRight(hostName, ".")
+	replacer := strings.NewReplacer(".", "_", "/", "_", "\\", "_", ":", "_", "%", "_")
+	hostName = replacer.Replace(hostName)
+	return hostName
 }
 
+func checkIgnoreMetric(mkey string) bool {
+	IgnoreChildMetric := "server"
+	IgnoreMetric := ""
+	ret := false
+	if strings.EqualFold(IgnoreChildMetric, "nil") == false {
+		subMetric := strings.Split(mkey, "/")
+		if strings.Contains(IgnoreChildMetric, subMetric[len(subMetric)-1]) == true {
+			ret = true
+		}
+	}
+	if strings.EqualFold(IgnoreMetric, "nil") == false {
+		if strings.Contains(IgnoreMetric, mkey) == true {
+			ret = true
+		}
+	}
 
-func checkIgnoreMetric(mkey string)(bool) {
-    IgnoreChildMetric := "server"
-    IgnoreMetric := ""
-    ret := false
-    if strings.EqualFold(IgnoreChildMetric,"nil") == false {
-        subMetric := strings.Split(mkey,"/")
-        if strings.Contains(IgnoreChildMetric,subMetric[len(subMetric)-1]) == true {
-            ret = true
-        }
-    }
-    if strings.EqualFold(IgnoreMetric,"nil") == false {
-        if strings.Contains(IgnoreMetric,mkey) == true {
-            ret = true
-        }
-    }
-
-    return ret
+	return ret
 }
 
-func getNamespace(mkey string)(ns core.Namespace) {
+func getNamespace(mkey string) (ns core.Namespace) {
 
-    rc := strings.Replace(mkey,".","-",-1)
-    ss := strings.Split(rc,"/")
-    ns = core.NewNamespace(ss[0])
-    for i:=1; i < len(ss); i++ {
-        ns = ns.AddStaticElement(ss[i])
-    }
-    return ns 
+	rc := strings.Replace(mkey, ".", "-", -1)
+	ss := strings.Split(rc, "/")
+	ns = core.NewNamespace(ss[0])
+	for i := 1; i < len(ss); i++ {
+		ns = ns.AddStaticElement(ss[i])
+	}
+	return ns
 }
 
 func switchType(outMetric *[]plugin.MetricType, mval interface{}, ak string) {
 
-    switch mtype := mval.(type) {
-    case bool:
-        if checkIgnoreMetric(ak) == true {
-            return
-        }
-        if mval == false { 
-          ns := getNamespace(ak) 
-          tmp := plugin.MetricType{
-            Namespace_:ns,
-            Data_:0,
-            Timestamp_:time.Now(),
-          }     
-          *outMetric = append(*outMetric,tmp)
-        } else {
-          ns := getNamespace(ak) 
-          tmp := plugin.MetricType{
-            Namespace_:ns,
-            Data_:1,
-            Timestamp_:time.Now(),
-          }     
-          *outMetric = append(*outMetric,tmp)
-        }
-    case int, int64, float64, string:
-        if checkIgnoreMetric(ak) == true {
-            return
-        }
-        ns := getNamespace(ak) 
-        tmp := plugin.MetricType{
-            Namespace_:ns,
-            Data_:mval,
-            Timestamp_:time.Now(),
-        }     
-        *outMetric = append(*outMetric,tmp)
-    case map[string]interface{}:
-        parseMetrics(outMetric, mtype, ak)
-    case []interface{}:
-        parseArrMetrics(outMetric, mtype, ak)
-    default:
-      fmt.Println("In default missing type =",reflect.TypeOf(mval))
-    }
-    return
+	switch mtype := mval.(type) {
+	case bool:
+		if checkIgnoreMetric(ak) == true {
+			return
+		}
+		if mval == false {
+			ns := getNamespace(ak)
+			tmp := plugin.MetricType{
+				Namespace_: ns,
+				Data_:      0,
+				Timestamp_: time.Now(),
+			}
+			*outMetric = append(*outMetric, tmp)
+		} else {
+			ns := getNamespace(ak)
+			tmp := plugin.MetricType{
+				Namespace_: ns,
+				Data_:      1,
+				Timestamp_: time.Now(),
+			}
+			*outMetric = append(*outMetric, tmp)
+		}
+	case int, int64, float64, string:
+		if checkIgnoreMetric(ak) == true {
+			return
+		}
+		log.Println(ak)
+		log.Println(mval)
+		ns := getNamespace(ak)
+		tmp := plugin.MetricType{
+			Namespace_: ns,
+			Data_:      mval,
+			Timestamp_: time.Now(),
+		}
+		log.Println(tmp)
+		*outMetric = append(*outMetric, tmp)
+	case map[string]interface{}:
+		parseMetrics(outMetric, mtype, ak)
+	case []interface{}:
+		parseArrMetrics(outMetric, mtype, ak)
+	default:
+		log.Println("In default missing type =", reflect.TypeOf(mval))
+	}
+	return
 }
 
-func parseArrMetrics( outMetric *[]plugin.MetricType, inData []interface{}, parentKey string)  {
-    for mkey, mval := range inData {
-        //fmt.Println("mkey =", mkey,"mval =",mval,"inData =",inData,"parentKey =",parentKey)
-        subMetric := strings.Split(parentKey,"/")
-        if subMetric[len(subMetric)-1] == "peers" {
-            hostName:= getHostName(mval, strconv.Itoa(mkey))
-            //fmt.Println("hostName =",hostName)
-            switchType(outMetric, mval, parentKey+"/"+hostName)
-        }else {
-            switchType(outMetric, mval, parentKey+ "/"+ strconv.Itoa(mkey))
-        }
-    }
-    return
+func parseArrMetrics(outMetric *[]plugin.MetricType, inData []interface{}, parentKey string) {
+	for mkey, mval := range inData {
+		subMetric := strings.Split(parentKey, "/")
+		if subMetric[len(subMetric)-1] == "peers" {
+			hostName := getHostName(mval, strconv.Itoa(mkey))
+			switchType(outMetric, mval, parentKey+"/"+hostName)
+		} else {
+			switchType(outMetric, mval, parentKey+"/"+strconv.Itoa(mkey))
+		}
+	}
+	return
 }
 
+func parseMetrics(outMetric *[]plugin.MetricType, inData map[string]interface{}, parentKey string) {
 
-func parseMetrics( outMetric *[]plugin.MetricType, inData map[string]interface{}, parentKey string)  {
+	for mkey, mval := range inData {
+		switchType(outMetric, mval, parentKey+"/"+mkey)
+	}
 
-    for mkey, mval := range inData {
-        switchType(outMetric, mval, parentKey + "/" + mkey)
-    }
-    return
+	return
 }
-
 
 func getMetrics(webserver string, metrics []string) (mts []plugin.MetricType, err error) {
-    tr := &http.Transport{}
-    client := &http.Client{Transport: tr}
-    resp, err1 := client.Get(webserver)
-    if err1 != nil {
-        return nil, err1
-    }
-    if resp.StatusCode != 200 {
-        defer resp.Body.Close()
-        return nil, errReqFailed
-    }
-    body, err2 := ioutil.ReadAll(resp.Body)
-    if err2 != nil {
-        return nil,err2
-    }
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr}
+	resp, err1 := client.Get(webserver)
+	if err1 != nil {
+		return nil, err1
+	}
+	if resp.StatusCode != 200 {
+		defer resp.Body.Close()
+		return nil, errReqFailed
+	}
+	body, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		return nil, err2
+	}
 
-    //fmt.Println("Before Unmarshal")
- 
-    jFmt := make(map[string]interface{})
+	jFmt := make(map[string]interface{})
 
-    err = json.Unmarshal(body, &jFmt)
-    if err != nil {
-        return nil, err
-    }
+	err = json.Unmarshal(body, &jFmt)
+	if err != nil {
+		return nil, err
+	}
 
-    //fmt.Println("jFmt =",jFmt)
+	pk := "staples" + "/" + "nginx"
+	parseMetrics(&mts, jFmt, pk)
 
-    pk :="staples"+"/"+"nginx"
-    parseMetrics(&mts,jFmt,pk)
+	// var names []string
+	// ns.FromJSON(&body, "staples", &names)
+	// log.Println(strings.Join(names, "\n"))
 
-    fmt.Println("After calling parseMetrics\n")
+	// log.Println(mts)
 
-    return mts, nil
+	return mts, nil
 }
 
-func (n *Nginx) CollectMetrics(inmts []plugin.MetricType) (mts []plugin.MetricType, err error) {
-    webservercfg := inmts[0].Config().Table()
+func (n *Nginx) CollectMetrics(inmts []plugin.MetricType) ([]plugin.MetricType, error) {
+	webservercfg := inmts[0].Config().Table()["nginx_status_url"]
 
-    for _,v := range webservercfg {
-        webserver, ok := v.(ctypes.ConfigValueStr)
-        if !ok {
-            return nil, errBadWebserver
-        }
+	webserver, ok := webservercfg.(ctypes.ConfigValueStr)
+	if !ok {
+		return nil, errBadWebserver
+	}
 
-        metrics := make([]string, len(inmts))
+	metrics := make([]string, len(inmts))
 
-        for i, m := range inmts {
-            metrics[i] = m.Namespace().String()
-        }
+	for i, m := range inmts {
+		metrics[i] = m.Namespace().String()
+	}
 
-        mts, err = getMetrics(webserver.Value, metrics)
+	mts, err := getMetrics(webserver.Value, metrics)
 
-        if err != nil {
-            fmt.Println("Error in Get Metric =",err) 
-        } 
-    }
+	if err != nil {
+		log.Println("Error in Get Metric =", err)
+	}
 
-    fmt.Println("CollectMetrics =",mts) 
+	var returnMets []plugin.MetricType
+	for _, m := range mts {
+		log.Println(m)
+		returnMets = append(returnMets, m)
+	}
 
-    return mts,nil
+	return returnMets, nil
 }
 
-func (n *Nginx) GetMetricTypes(cfg plugin.ConfigType) (mts []plugin.MetricType, err error) {
-    webservercfg := cfg.Table()
-    if webservercfg == nil {
-        return nil,errConfigReadError
-    }
+func (n *Nginx) GetMetricTypes(cfg plugin.ConfigType) ([]plugin.MetricType, error) {
+	webservercfg := cfg.Table()["nginx_status_url"]
+	if webservercfg == nil {
+		return nil, errConfigReadError
+	}
 
-    for _,v := range webservercfg {
-        webserver, ok := v.(ctypes.ConfigValueStr)
-        if !ok {
-            return nil, errBadWebserver
-        }
+	webserver, ok := webservercfg.(ctypes.ConfigValueStr)
+	if !ok {
+		return nil, errBadWebserver
+	}
 
-        mts, err = getMetrics(webserver.Value, []string{})
-        if err != nil {
-            fmt.Println("Error in Get Metric =",err) 
-        } 
-    }
+	mts, err := getMetrics(webserver.Value, []string{})
 
-    fmt.Println("GetMetricTypes Metrics =",mts) 
-    return mts, nil
+	if err != nil {
+		log.Println("Error in Get Metric =", err)
+	}
+
+	var returnMets []plugin.MetricType
+	for _, m := range mts {
+		log.Println(m)
+		returnMets = append(returnMets, m)
+	}
+
+	return returnMets, nil
 }
 
 func (n *Nginx) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
-    cfg := cpolicy.New()
-    nginxrule,_ := cpolicy.NewStringRule("nginx_status_url", false ,"http://demo.nginx.com/status")
-    policy := cpolicy.NewPolicyNode()
-    policy.Add(nginxrule)
-    cfg.Add([]string{"staples","nginx"},policy)
-    return cfg, nil
+	cfg := cpolicy.New()
+	nginxrule, _ := cpolicy.NewStringRule("nginx_status_url", false, "http://demo.nginx.com/status")
+	policy := cpolicy.NewPolicyNode()
+	policy.Add(nginxrule)
+	cfg.Add([]string{"staples", "nginx"}, policy)
+	return cfg, nil
 }
 
 func Meta() *plugin.PluginMeta {
-    return plugin.NewPluginMeta(
-        Name,
-        Version,
-        Type,
-        []string{plugin.SnapGOBContentType},
-        []string{plugin.SnapGOBContentType},
-        plugin.Unsecure(true),
-        plugin.RoutingStrategy(plugin.DefaultRouting),
-        plugin.CacheTTL(1100*time.Millisecond),
-    )
+	return plugin.NewPluginMeta(
+		Name,
+		Version,
+		Type,
+		[]string{plugin.SnapGOBContentType},
+		[]string{plugin.SnapGOBContentType},
+		plugin.Unsecure(true),
+		plugin.RoutingStrategy(plugin.DefaultRouting),
+		plugin.CacheTTL(1100*time.Millisecond),
+	)
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 go install ./...
-$GOPATH/bin/snap-v0.14.0-beta/bin/snapctl plugin load $GOPATH/bin/snap-plugin-collector-nginx
-$GOPATH/bin/snap-v0.14.0-beta/bin/snapctl plugin load $GOPATH/bin/snap-plugin-processor-staplesfmt
-$GOPATH/bin/snap-v0.14.0-beta/bin/snapctl plugin load $GOPATH/bin/snap-plugin-publisher-metrics
-$GOPATH/bin/snap-v0.14.0-beta/bin/snapctl task create -t $GOPATH/src/github.com/intelsdi-x/snap-plugin-collector-nginx/examples/tasks/task.json
+snapctl plugin load $GOPATH/bin/snap-plugin-collector-nginx
+snapctl plugin load $GOPATH/bin/snap-plugin-processor-staplesfmt
+snapctl plugin load $GOPATH/bin/snap-plugin-publisher-metrics
+snapctl task create -t $GOPATH/src/github.com/intelsdi-x/snap-plugin-collector-nginx/examples/tasks/task.json
