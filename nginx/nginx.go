97bcdff0c61d7d4b48ab769e3bed55b1a9906a56
Sattara Natarajan, Sundara Senthil <SundaraSenthil.SattaraNatarajan@Staples.com>
2016-06-28 21:06:17 -0400

    Improved code quality

--- nginx/nginx.go
+++ nginx/nginx.go
@@ -49,32 +49,36 @@ func getMD5Hash(text string) string {
 }
 
 func getHostName(inData interface{}, hostName string) string {
-	hostName = fmt.Sprintf("host_id_%s", hostName)
+        flag := false
 	switch mtype := inData.(type) {
-	case map[string]interface{}:
-		val := mtype["server"].(string)
-		//Default hostname with port will be encoded to md5
-		//If LookUpAddr get valid hostname it will be replace
-		hostName = fmt.Sprintf("host_id_%s", getMD5Hash(val))
-
+	   case map[string]interface{}:
+	        hostName = mtype["server"].(string)
 		//check for IPV4
-		if strings.Count(val, ".") == 3 {
-			subStr := strings.Split(val, ":")
+		if strings.Count(hostName, ".") == 3 {
+			subStr := strings.Split(hostName, ":")
 			hName, err := net.LookupAddr(subStr[0])
 			if err == nil {
 				hostName = strings.Join(hName, ".")
+                                flag = true
 			}
 		} else {
-			if strings.Contains(val, "::") == true {
-				subStr := strings.Split(val, "]")
+			if strings.Contains(hostName, "::") == true {
+				subStr := strings.Split(hostName, "]")
 				tStr := strings.TrimLeft(subStr[0], "[")
 				hName, err := net.LookupAddr(tStr)
 				if err == nil {
 					hostName = strings.Join(hName, ".")
+                                        flag = true
 				}
 			}
 		}
 	}
+        if flag == false {
+	  //Default hostname with port will be encoded to md5
+	  hostName = fmt.Sprintf("host_id_%s", getMD5Hash(hostName))
+        } else {
+	  hostName = fmt.Sprintf("host_id_%s", hostName)
+        }
 	hostName = strings.TrimRight(hostName, ".")
 	replacer := strings.NewReplacer(".", "_", "/", "_", "\\", "_", ":", "_", "%", "_")
 	hostName = replacer.Replace(hostName)
@@ -104,52 +108,40 @@ func getNamespace(mkey string) (ns core.Namespace) {
 
 	rc := strings.Replace(mkey, ".", "-", -1)
 	ss := strings.Split(rc, "/")
-	ns = core.NewNamespace(ss[0])
-	for i := 1; i < len(ss); i++ {
-		ns = ns.AddStaticElement(ss[i])
-	}
+	ns = core.NewNamespace(ss...)
 	return ns
 }
 
 func switchType(outMetric *[]plugin.MetricType, mval interface{}, ak string) {
-
 	switch mtype := mval.(type) {
 	case bool:
 		if checkIgnoreMetric(ak) == true {
 			return
 		}
-		if mval == false {
-			ns := getNamespace(ak)
-			tmp := plugin.MetricType{
-				Namespace_: ns,
-				Data_:      0,
-				Timestamp_: time.Now(),
-			}
-			*outMetric = append(*outMetric, tmp)
-		} else {
-			ns := getNamespace(ak)
-			tmp := plugin.MetricType{
-				Namespace_: ns,
-				Data_:      1,
-				Timestamp_: time.Now(),
-			}
-			*outMetric = append(*outMetric, tmp)
-		}
+                ns := getNamespace(ak)
+		tmp := plugin.MetricType{}
+		tmp.Namespace_= ns
+                if mval.(bool) == false {
+		   tmp.Data_= 0 
+                } else {
+		   tmp.Data_= 1
+                }
+		tmp.Timestamp_= time.Now()
+		*outMetric = append(*outMetric, tmp)
 	case int, int64, float64, string:
 		if checkIgnoreMetric(ak) == true {
 			return
 		}
-		ns := getNamespace(ak)
-		tmp := plugin.MetricType{
-			Namespace_: ns,
-			Data_:      mval,
-			Timestamp_: time.Now(),
-		}
+                ns := getNamespace(ak)
+		tmp := plugin.MetricType{}
+		tmp.Namespace_= ns
+		tmp.Data_=      mval
+		tmp.Timestamp_= time.Now()
 		*outMetric = append(*outMetric, tmp)
 	case map[string]interface{}:
 		parseMetrics(outMetric, mtype, ak)
 	case []interface{}:
-		parseArrMetrics(outMetric, mtype, ak)
+        	parseArrMetrics(outMetric, mtype, ak)
 	default:
 		log.Println("In default missing type =", reflect.TypeOf(mval))
 	}
@@ -161,7 +153,7 @@ func parseArrMetrics(outMetric *[]plugin.MetricType, inData []interface{}, paren
 		subMetric := strings.Split(parentKey, "/")
 		if subMetric[len(subMetric)-1] == "peers" {
 			hostName := getHostName(mval, strconv.Itoa(mkey))
-			switchType(outMetric, mval, parentKey+"/"+hostName)
+		        switchType(outMetric, mval, parentKey+"/"+hostName)
 		} else {
 			switchType(outMetric, mval, parentKey+"/"+strconv.Itoa(mkey))
 		}
@@ -229,7 +221,7 @@ func (n *Nginx) CollectMetrics(inmts []plugin.MetricType) ([]plugin.MetricType, 
         return mts, nil   
 }
 
-func (n *Nginx) GetMetricTypes(cfg plugin.ConfigType) ([]plugin.MetricType, error) {
+func (n *Nginx) GetMetricTypes(cfg plugin.ConfigType) (mts []plugin.MetricType, err error) {
 	webservercfg := cfg.Table()["nginx_status_url"]
 	if webservercfg == nil {
 		return nil, errConfigReadError
@@ -240,7 +232,7 @@ func (n *Nginx) GetMetricTypes(cfg plugin.ConfigType) ([]plugin.MetricType, erro
 		return nil, errBadWebserver
 	}
 
-	mts, err := getMetrics(webserver.Value, []string{})
+	mts, err = getMetrics(webserver.Value, []string{})
 
 	if err != nil {
 		log.Println("Error in Get Metric =", err)
